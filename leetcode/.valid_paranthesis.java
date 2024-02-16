c645b84f4eea42d028abe2d32647766da06b4d75
Nibinp <87687660+Nibinp@users.noreply.github.com>
2024-02-16 21:05:05 +0530

    Update .valid_paranthesis.java

--- leetcode/.valid_paranthesis.java
+++ leetcode/.valid_paranthesis.java
@@ -1,1 +1,50 @@
 
+// the solution for leetcode problem "valid paranthesis"
+public boolean isValid(String str) {
+         Stack<Character> stack = new Stack<Character>();
+        
+	    for(int i=0;i<str.length();i++)
+	    {
+	        
+	        if(str.charAt(i)=='('||str.charAt(i)=='{'||str.charAt(i)=='[')  
+	        {
+	            stack.push(str.charAt(i));
+	          
+	        }  
+	        else if(str.charAt(i)==')'||str.charAt(i)=='}'||str.charAt(i)==']')
+	        {
+	        	if(stack.empty())
+	        	{
+	        		stack.push(' ');
+	        		break;
+	        	}
+
+	        	if(!stack.empty())
+	        	{
+	        	if(str.charAt(i)==(char)(stack.peek()+1) || str.charAt(i)==(char)(stack.peek()+2))
+	        	{
+	        		stack.pop();
+	        	}
+	        	else
+	        	{
+	        		return false;
+	        	}
+	        }
+	        
+	       
+	       
+	    }
+		
+	    }
+	    
+	    if(stack.empty())
+	    {
+	    	return true;
+	    }
+	    else
+	    {
+	    	return false;
+	    }
+		
+		
+}
